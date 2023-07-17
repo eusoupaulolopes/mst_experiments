6628e35e46683f81cd8268c3b7c18be1d20598c6
Paulo <paulo.lopes.013@ufrn.edu.br>
2023-07-17 12:51:37 -0300

    clean some code

--- provider-node/core/resource.py
+++ provider-node/core/resource.py
@@ -1,8 +1,113 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from enum import Enum
+import time
 
-class Resource(ABC):
+CAPACITY= 1000
+CHARGE=1000
+RECOVER_CAPACITY = -2
+
+class OPMODE(Enum):
+    IDLE, ACTIVE, DOWN = 'Idle','Ativo','Descarregado'
+   
+
+class Resource(ABC):                
+    def __init__(self):
+        self.capacity = CAPACITY
+        self.charge = CHARGE
+        self.mode = OPMODE.IDLE
+        self.log = []
+   
+    def get_charge(self) -> int:
+        return self.charge
     
     @abstractmethod
-    def consume(self):
+    def consume(self) -> None:
         pass
     
+    def reload(self) -> None:
+        self.charge = 1000
+    
+    def get_logs(self) -> list:
+        for l in self.log:
+            print(l)
+        return self.log
+    
+    def log_entry(self, adv:str = None) -> None:
+        self.log.append(f"Operando em {self.mode.value}, carga atual: {self.charge}")
+        
+    
+class SimpleResource(Resource):        
+   
+    def consume(self):
+        while self.charge > 0:            
+            self.log_entry()
+            demmand = 3
+            if self.mode == OPMODE.ACTIVE:
+                demmand += 3
+                
+            self.charge = self.charge - demmand
+                        
+            if self.charge >= self.capacity:
+                self.charge = self.capacity    
+            time.sleep(1)
+            
+        if self.charge <= 0:
+            self.mode = OPMODE.DOWN
+            self.log_entry("Battery Empty)")
+
+            
+class ResourceWithRecovery(Resource):       
+   
+   def consume(self):
+        idle_time = 0
+        while self.charge > 0:            
+            self.log_entry()
+            demmand = 3
+            if self.mode == OPMODE.ACTIVE:
+                demmand += 3
+                idle_time = 0    
+                            
+            if self.mode == OPMODE.IDLE:
+                idle_time+=1
+                if idle_time >= 3:
+                    demmand = RECOVER_CAPACITY
+                
+            self.charge = self.charge - demmand 
+            
+            if self.charge >= self.capacity: 
+                self.charge = self.capacity    
+            time.sleep(1)
+        
+        if self.charge <= 0:
+            self.mode = OPMODE.DOWN
+            self.log_entry("Battery Empty)")
+        idle_time = 0
+
+
+
+
+def consume(self):
+        idle_time = 0
+        while True:            
+            self.log_charge()
+            demmand = 3
+            if self.mode == OPMODE.ACTIVE:
+                demmand += 3
+                idle_time = 0    
+                            
+            if self.mode == OPMODE.IDLE:
+                idle_time+=1
+                if idle_time >= 3:
+                    demmand = RECOVER_CAPACITY
+                
+            self.charge = self.charge - demmand
+            
+            if self.charge <= 0:
+                self.mode = OPMODE.DOWN
+                self.log_charge("Battery Empty)")
+                idle_time = 0
+                self.charge = 0
+            elif self.charge >= self.capacity:
+                self.charge = self.capacity    
+            time.sleep(1)
--- provider-node/core/sensor.py
+++ provider-node/core/sensor.py
@@ -1,86 +1,33 @@
 import random, time, uuid, threading
-import resource
+from resource import Resource, ResourceWithRecovery
 from enum import Enum
 from typing import List
 import asyncio
 
-
-CAPACITY= 1000
-CHARGE=1000
-RECOVER_CAPACITY = -2
-
 class OPMODE(Enum):
     IDLE, ACTIVE, DOWN = 'Idle','Active','Descarregado'
-    
-class Battery:
-            
-    def __init__(self):
-        self.capacity = CAPACITY
-        self.charge = CHARGE
-        self.mode = OPMODE.IDLE
-        self.log = []
-   
-    def get_charge(self):
-        return self.charge
-    
-    def consume(self):
-        idle_time = 0
-        while True:            
-            self.log_charge()
-            demmand = 3
-            if self.mode == OPMODE.ACTIVE:
-                demmand += 3
-                idle_time = 0    
-                            
-            if self.mode == OPMODE.IDLE:
-                idle_time+=1
-                if idle_time >= 3:
-                    demmand = RECOVER_CAPACITY
-                
-            self.charge = self.charge - demmand
-            
-            if self.charge <= 0:
-                self.mode = OPMODE.DOWN
-                self.log_charge("Battery Empty)")
-                idle_time = 0
-                self.charge = 0
-            elif self.charge >= self.capacity:
-                self.charge = self.capacity    
-            time.sleep(1)
-    
-    def reload(self):
-        self.charge = 1000
-    
-    def get_battery_logs(self):
-        for l in self.log:
-            print(l)
-        return self.log
-    
-    def log_charge(self, adv:str = None):
-        self.log.append(f"Operando em {self.mode.value}, carga atual: {self.charge}")
-        
 
 class Sensor:            
      
-    def __init__(self, name:str):
+    def __init__(self, name:str, resource:Resource = ResourceWithRecovery):
         self.id=uuid.uuid1()
         self.name=name
-        self.battery=Battery()
+        self.resource=resource()
         self.status=OPMODE.DOWN   
         
     def power_on(self):
         print(f"ligando sensor {self.name}")
         self.status=OPMODE.IDLE  
-        self.thr = threading.Thread(target=self.battery.consume)
+        self.thr = threading.Thread(target=self.resource.consume)
         self.thr.daemon = True
         self.thr.start()
     
     def switch_status(self, status:OPMODE):
         self.status=status
-        self.battery.mode=status   
+        self.resource.mode=status   
         
     def get_measure(self) -> int: 
-        if self.battery.mode == OPMODE.DOWN:
+        if self.resource.mode == OPMODE.DOWN:
             self.status == OPMODE.DOWN
             raise Exception("Sensor descarregado.") 
         else:              
@@ -94,5 +41,5 @@ class Sensor:
         return {
             "sensor_id": self.id,
             "sensor_name": self.name,
-            "sensor_charge": f'{self.battery.charge} unids'
+            "sensor_charge": f'{self.resource.charge} unids'
         }
